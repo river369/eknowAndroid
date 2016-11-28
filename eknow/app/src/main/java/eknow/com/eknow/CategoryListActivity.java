a84d3605c4affbf930a03a9e98dc70d4ccb64282
Gao <jianguog@a0999b04f99b.ant.amazon.com>
2016-11-28 23:23:27 +0800

    add a grid view with some images

---
+++ eknow/app/src/main/java/eknow/com/eknow/CategoryListActivity.java
@@ -0,0 +1,22 @@
+package eknow.com.eknow;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by jianguog on 16/11/28.
+ */
+
+public class CategoryListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_list);
+
+    }
+
+}
--- eknow/app/src/main/java/eknow/com/eknow/MainActivity.java
+++ eknow/app/src/main/java/eknow/com/eknow/MainActivity.java
@@ -1,7 +1,13 @@
 package eknow.com.eknow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                sendMessage();
+                Toast.makeText(MainActivity.this, id + "  " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage() {
+        Intent intent = new Intent(this,  CategoryListActivity.class);
+        startActivity(intent);
     }
 }

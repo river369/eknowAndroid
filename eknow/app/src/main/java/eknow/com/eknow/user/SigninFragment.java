a64ec5fbc6a474308bb2360115f8aaa5191b50f8
System Administrator <root@amazon.com>
2016-12-29 18:28:49 +0800

    add validation library

--- eknow/app/src/main/java/eknow/com/eknow/user/SigninFragment.java
+++ eknow/app/src/main/java/eknow/com/eknow/user/SigninFragment.java
@@ -1,10 +1,13 @@
 package eknow.com.eknow.user;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import eknow.com.eknow.FragmentsFactory;
@@ -12,8 +15,26 @@ import eknow.com.eknow.MainActivity;
 import eknow.com.eknow.R;
 import eknow.com.eknow.common.BaseFragment;
 
+import eu.inmite.android.lib.validations.form.FormValidator;
+import eu.inmite.android.lib.validations.form.annotations.DateInFuture;
+import eu.inmite.android.lib.validations.form.annotations.Joined;
+import eu.inmite.android.lib.validations.form.annotations.MinLength;
+import eu.inmite.android.lib.validations.form.annotations.MinValue;
+import eu.inmite.android.lib.validations.form.annotations.NotEmpty;
+import eu.inmite.android.lib.validations.form.annotations.RegExp;
+import eu.inmite.android.lib.validations.form.callback.SimpleErrorPopupCallback;
+
+import static eu.inmite.android.lib.validations.form.annotations.RegExp.EMAIL;
+
 public class SignInFragment extends BaseFragment {
     View view;
+    @NotEmpty(messageId = R.string.validation_name, order = 1)
+    @MinLength(value = 3, messageId = R.string.validation_name_length, order = 1)
+    EditText phoneNumber;
+
+    @NotEmpty(messageId = R.string.validation_name, order = 2)
+    @MinLength(value = 3, messageId = R.string.validation_name_length, order = 2)
+    EditText password;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -28,10 +49,15 @@ public class SignInFragment extends BaseFragment {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentsFactory.setFragmentTo(getActivity(),((MainActivity)getActivity()).getCurrentFragment());
+                validate();
+                //Signin();
+                //FragmentsFactory.setFragmentTo(getActivity(),((MainActivity)getActivity()).getCurrentFragment());
             }
         });
 
+        phoneNumber = (EditText) view.findViewById(R.id.signin_phone_number);
+        password = (EditText) view.findViewById(R.id.signin_password);
+        //phoneNumber.setError();
         TextView signOn = (TextView) view.findViewById(R.id.signOn);
         signOn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +65,16 @@ public class SignInFragment extends BaseFragment {
                 FragmentsFactory.getInstance().setSignOnFragment(getActivity(), SignInFragment.this, null);
             }
         });
+        //FormValidator.startLiveValidation(this, view, new SimpleErrorPopupCallback(getActivity(), false));
+
         return view;
     }
+
+    private void validate() {
+        final boolean isValid = FormValidator.validate(this, new SimpleErrorPopupCallback(getActivity(), true));
+
+    }
+
+
+
 }

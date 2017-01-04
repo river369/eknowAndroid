d4cd61a4be1561f92bbb203fa5357a165a7e8d9d
System Administrator <root@amazon.com>
2017-01-04 19:16:26 +0800

    test a upload picture

--- eknow/app/src/main/java/eknow/com/eknow/service/ServiceAddFragment.java
+++ eknow/app/src/main/java/eknow/com/eknow/service/ServiceAddFragment.java
@@ -1,32 +1,47 @@
 package eknow.com.eknow.service;
 
-import android.app.DatePickerDialog;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.InputType;
-import android.text.TextWatcher;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
-
-import eknow.com.eknow.KeyConstants;
 import eknow.com.eknow.MainActivity;
 import eknow.com.eknow.R;
-import eknow.com.eknow.ValueConstants;
 import eknow.com.eknow.common.BaseFragment;
-import me.gujun.android.taggroup.TagGroup;
+import eknow.com.eknow.common.photo.activity.AlbumActivity;
+import eknow.com.eknow.common.photo.activity.GalleryActivity;
+import eknow.com.eknow.common.photo.util.Bimp;
+import eknow.com.eknow.common.photo.util.FileUtils;
+import eknow.com.eknow.common.photo.util.ImageItem;
+import eknow.com.eknow.common.photo.util.PublicWay;
+import eknow.com.eknow.common.photo.util.Res;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by jianguog on 16/11/28.
@@ -38,6 +53,20 @@ import me.gujun.android.taggroup.TagGroup;
 public class ServiceAddFragment extends BaseFragment {
 
     View view;
+    View popView;
+//    private GridView picturesGridView;
+//    private Bitmap pictureBitmap;
+//    private ArrayList<HashMap<String, Object>> imageItems;
+//    private SimpleAdapter simpleAdapter;
+//    private final int IMAGE_OPEN = 1;
+//    private String pathImage;
+
+    private GridView noScrollgridview;
+    private GridAdapter adapter;
+    private PopupWindow pop = null;
+    private LinearLayout ll_popup;
+    public static Bitmap bimap ;
+
     EditText serviceHourText;
     EditText servicePeopleText;
     EditText totalPriceText;
@@ -49,9 +78,300 @@ public class ServiceAddFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ((MainActivity) getActivity()).setToolbarTitle(R.string.serviceAdd);
         view = inflater.inflate(R.layout.service_add, container, false);
-        TagGroup mTagGroup = (TagGroup) view.findViewById(R.id.tag_group);
-        mTagGroup.setTags(new String[]{"Tag1", "Tag2", "Tag3"});
+        popView = inflater.inflate(R.layout.select_picture_popup, container, false);
+//        TagGroup mTagGroup = (TagGroup) view.findViewById(R.id.tag_group);
+//        mTagGroup.setTags(new String[]{"Tag1", "Tag2", "Tag3"});
+
+        InitPictures();
+        /*
+        picturesGridView = (GridView) view.findViewById(R.id.service_upload_pictures);
+        pictureBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.addpic);
+        imageItems = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("itemImage", pictureBitmap);
+        imageItems.add(map);
+        simpleAdapter = new SimpleAdapter(getActivity(),
+                imageItems, R.layout.griditem_addpic,
+                new String[] { "itemImage"}, new int[] { R.id.imageView1});
+
+        simpleAdapter.setViewBinder(new ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Object data, String textRepresentation) {
+                if(view instanceof MLImageView && data instanceof Bitmap){
+                    ImageView i = (ImageView)view;
+                    i.setImageBitmap((Bitmap) data);
+                    return true;
+                }
+                return false;
+            }
+        });
+        picturesGridView.setAdapter(simpleAdapter);
+
+        picturesGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                System.out.println(position);
+                if( imageItems.size() == 6) {
+                    Toast.makeText(getActivity(), "no more than 5", Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 0) {
+                    Toast.makeText(getActivity(), "start new ones", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, IMAGE_OPEN);
+                }
+                else {
+                    //dialog(position);
+                    Toast.makeText(getActivity(), position + 1,
+                    		Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        */
+
+
         return view;
+    }
+
+    public void InitPictures() {
+
+        Res.init(getActivity());
+        PublicWay.activityList.add(getActivity());
+
+        pop = new PopupWindow(getActivity());
+
+        ll_popup = (LinearLayout) popView.findViewById(R.id.select_picture_popup);
+
+        pop.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        pop.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        pop.setBackgroundDrawable(new BitmapDrawable());
+        pop.setFocusable(true);
+        pop.setOutsideTouchable(true);
+        pop.setContentView(popView);
+
+        RelativeLayout parent = (RelativeLayout) popView.findViewById(R.id.select_picture_popup_parent);
+
+        Button bt1 = (Button) popView
+                .findViewById(R.id.item_popupwindows_camera);
+        Button bt2 = (Button) popView
+                .findViewById(R.id.item_popupwindows_Photo);
+        Button bt3 = (Button) popView
+                .findViewById(R.id.item_popupwindows_cancel);
+
+        parent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+        bt1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                photo();
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AlbumActivity.class);
+                startActivity(intent);
+                //overridePendingTransition(R.anim.activity_translate_in, R.anim.activity_translate_out);
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pop.dismiss();
+                ll_popup.clearAnimation();
+            }
+        });
+
+        noScrollgridview = (GridView) view.findViewById(R.id.service_upload_pictures);
+        noScrollgridview.setSelector(new ColorDrawable(Color.TRANSPARENT));
+        adapter = new GridAdapter(getActivity());
+        adapter.update();
+        noScrollgridview.setAdapter(adapter);
+        noScrollgridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                if (arg2 == Bimp.tempSelectBitmap.size()) {
+                    Log.i("ddddddd", "----------");
+
+                    //ll_popup.startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.activity_translate_in));
+                    pop.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+                } else {
+                    Intent intent = new Intent(getActivity(), GalleryActivity.class);
+                    intent.putExtra("position", "1");
+                    intent.putExtra("ID", arg2);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    @SuppressLint("HandlerLeak")
+    public class GridAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
+        private int selectedPosition = -1;
+        private boolean shape;
+
+        public boolean isShape() {
+            return shape;
+        }
+
+        public void setShape(boolean shape) {
+            this.shape = shape;
+        }
+
+        public GridAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void update() {
+            loading();
+        }
+
+        public int getCount() {
+            if(Bimp.tempSelectBitmap.size() == 9){
+                return 9;
+            }
+            return (Bimp.tempSelectBitmap.size() + 1);
+        }
+
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        public void setSelectedPosition(int position) {
+            selectedPosition = position;
+        }
+
+        public int getSelectedPosition() {
+            return selectedPosition;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.item_published_grida,
+                        parent, false);
+                holder = new ViewHolder();
+                holder.image = (ImageView) convertView
+                        .findViewById(R.id.item_grida_image);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            if (position ==Bimp.tempSelectBitmap.size()) {
+                holder.image.setImageBitmap(BitmapFactory.decodeResource(
+                        getResources(), R.drawable.icon_addpic_unfocused));
+                if (position == 9) {
+                    holder.image.setVisibility(View.GONE);
+                }
+            } else {
+                holder.image.setImageBitmap(Bimp.tempSelectBitmap.get(position).getBitmap());
+            }
+
+            return convertView;
+        }
+
+        public class ViewHolder {
+            public ImageView image;
+        }
+
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1:
+                        adapter.notifyDataSetChanged();
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+        public void loading() {
+            new Thread(new Runnable() {
+                public void run() {
+                    while (true) {
+                        if (Bimp.max == Bimp.tempSelectBitmap.size()) {
+                            Message message = new Message();
+                            message.what = 1;
+                            handler.sendMessage(message);
+                            break;
+                        } else {
+                            Bimp.max += 1;
+                            Message message = new Message();
+                            message.what = 1;
+                            handler.sendMessage(message);
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
+
+    public String getString(String s) {
+        String path = null;
+        if (s == null)
+            return "";
+        for (int i = s.length() - 1; i > 0; i++) {
+            s.charAt(i);
+        }
+        return path;
+    }
+
+    protected void onRestart() {
+        adapter.update();
+        //super.onRestart();
+    }
+
+    private static final int TAKE_PICTURE = 0x000001;
+
+    public void photo() {
+        Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(openCameraIntent, TAKE_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (Bimp.tempSelectBitmap.size() < 9 && resultCode == RESULT_OK) {
+
+                    String fileName = String.valueOf(System.currentTimeMillis());
+                    Bitmap bm = (Bitmap) data.getExtras().get("data");
+                    FileUtils.saveBitmap(bm, fileName);
+
+                    ImageItem takePhoto = new ImageItem();
+                    takePhoto.setBitmap(bm);
+                    Bimp.tempSelectBitmap.add(takePhoto);
+                }
+                break;
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            for(int i = 0; i< PublicWay.activityList.size(); i++){
+                if (null != PublicWay.activityList.get(i)) {
+                    PublicWay.activityList.get(i).finish();
+                }
+            }
+            System.exit(0);
+        }
+        return true;
     }
 
 
